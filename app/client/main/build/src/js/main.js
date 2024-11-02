19bc172c0782ac4ba09ab0c9d075f35bafcfbcd7
Ben Nadel <ben@bennadel.com>
2024-11-02 15:08:15 -0400

    Adding initial client-side build code.

---
+++ app/client/main/build/src/js/main.js
@@ -0,0 +1,14 @@
+
+// Import vendor modules.
+import Alpine from "alpinejs";
+
+// Import app modules.
+import "../cfml-main/tags/**/*.view.js";
+import "../cfml-main/views/**/*.view.js";
+
+// ----------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+window.Alpine = Alpine;
+
+Alpine.start();
