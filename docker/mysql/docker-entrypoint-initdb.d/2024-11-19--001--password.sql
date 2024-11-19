3c3c513c9d569a9fbd808362abf05685850a4da2
Ben Nadel <ben@bennadel.com>
2024-11-19 08:54:45 -0500

    Once the code was in place, I can drop the default.

--- docker/mysql/docker-entrypoint-initdb.d/2024-11-19--001--password.sql
+++ docker/mysql/docker-entrypoint-initdb.d/2024-11-19--001--password.sql
@@ -4,3 +4,11 @@ ALTER TABLE
 ADD COLUMN
 	`password` VARCHAR(60) NOT NULL DEFAULT '' AFTER `videoUrl`
 ;
+
+-- Then added code, and now I can drop the default.
+
+ALTER TABLE
+	`incident`
+ALTER COLUMN
+	`password` DROP DEFAULT
+;
